4fca29dd9064beaa5416240e2b492e70802a4932
Wei-Jun Hung <weijunhung@outlook.com>
2023-02-06 19:11:37 +0800

    🎨 Catch error and replace `findOrCreate()` to prevent manually assign `createdAt`

--- src/commands/faucet.ts
+++ src/commands/faucet.ts
@@ -24,61 +24,70 @@ const FaucetModule = (cosmosClient: CosmosClient): Command => {
     );
 
   const onCommand = async (interaction: CommandInteraction) => {
-    await interaction.deferReply();
-
-    if (interaction.channelId !== Config.channelId) {
-      await interaction.editReply(
-        `:negative_squared_cross_mark: Please use this command in the dedicated channel: <#${Config.channelId}>`,
-      );
-      return;
-    }
-
-    const discordId = interaction.user.id;
     const address = interaction.options.getString('address', true);
-
     console.debug(`Token request to address ${address}`);
-    const [drop, created] = await sequelize.transaction(
-      {
-        isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-      },
-      async (t) => {
-        const result = await Drop.findOrCreate({
-          transaction: t,
-          where: {
-            createdAt: {
-              [Op.gt]: new Date(Date.now() - Config.faucet.cooldownInDay * 24 * 60 * 60 * 1000),
-            },
-            [Op.or]: [
-              { discordId: discordId },
-              { address: address },
-            ],
-          },
-          defaults: {
-            username: interaction.user.username,
-            discordId: discordId,
-            address: address,
-            amount: Config.faucet.amount,
-            txHash: 'pending',
-            createdAt: new Date(),
-          },
-        });
-        return result;
-      }
-    );
-
-    if (!created) {
-      const txHash = drop.get('txHash');
-      await interaction.editReply(`:negative_squared_cross_mark: Each user can only claim once, your previous txhash: [${txHash}](${Config.faucet.restUrl}/cosmos/tx/v1beta1/txs/${txHash}).`);
-      return;
-    }
-
     try {
-      const txHash = await cosmosClient.distribute(address);
-      await drop.update({ txHash: txHash });
-      await interaction.editReply(`:white_check_mark: Transaction submitted, txhash: [${txHash}](${Config.faucet.restUrl}/cosmos/tx/v1beta1/txs/${txHash})`);
+      await interaction.deferReply();
+
+      if (interaction.channelId !== Config.channelId) {
+        await interaction.editReply(
+          `:negative_squared_cross_mark: Please use this command in the dedicated channel: <#${Config.channelId}>`,
+        );
+        return;
+      }
+
+      const discordId = interaction.user.id;
+      const [drop, created] = await sequelize.transaction(
+        {
+          isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
+        },
+        async (t) => {
+          const pastDrop = await Drop.findOne({
+            transaction: t,
+            where: {
+              createdAt: {
+                [Op.gt]: new Date(Date.now() - Config.faucet.cooldownInDay * 24 * 60 * 60 * 1000),
+              },
+              [Op.or]: [
+                { discordId: discordId },
+                { address: address },
+              ],
+            },
+          });
+
+          if (pastDrop) {
+            return [pastDrop, false];
+          }
+
+          const newDrop = await Drop.create({
+              username: interaction.user.username,
+              discordId: discordId,
+              address: address,
+              amount: Config.faucet.amount,
+              txHash: 'pending',
+            },
+            { transaction: t },
+          );
+          return [newDrop, true];
+        }
+      )
+
+      if (!created) {
+        const txHash = drop.get('txHash');
+        await interaction.editReply(`:negative_squared_cross_mark: Each user can only claim once, your past txhash: [${txHash}](${Config.faucet.restUrl}/cosmos/tx/v1beta1/txs/${txHash}).`);
+        return;
+      }
+
+      try {
+        const txHash = await cosmosClient.distribute(address);
+        await drop.update({ txHash: txHash });
+        await interaction.editReply(`:white_check_mark: Transaction submitted, txhash: [${txHash}](${Config.faucet.restUrl}/cosmos/tx/v1beta1/txs/${txHash})`);
+      } catch (err: unknown) {
+        await drop.destroy();
+        throw err;
+      }
     } catch (err: unknown) {
       console.error(`Failed to create transaction to ${address} = `, err);
-      await drop.destroy();
       await interaction.editReply(`:warning: Failed to create transaction to \`${address}\`, please try again later`);
     }
   };
